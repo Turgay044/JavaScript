08b2013f12f3691893a8a92092fffb048387dda0
Turgay044 <97851445+Turgay044@users.noreply.github.com>
2022-03-06 17:13:59 +0300

    Add files via upload

--- FullStack-Project-Group/Colour Flipper/Script.js
+++ FullStack-Project-Group/Colour Flipper/Script.js
@@ -1,6 +1,9 @@
 document.getElementById("Button").addEventListener("click", changeColour);
 
+
 function changeColour(){   
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
     document.body.style.backgroundColor = "#" + randomColor;
+    document.getElementById("Span").textContent = "#" + randomColor;
 };
+
